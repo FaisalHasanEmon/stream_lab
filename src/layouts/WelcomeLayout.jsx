a0be7c7f4cbdf3fe3e424f5fbb7ed9b24c635619
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-08-24 17:45:10 +0600

    Welcome layout modified

--- src/layouts/WelcomeLayout.jsx
+++ src/layouts/WelcomeLayout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import NavBar from "../components/shared/Navbar/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/shared/Footer/Footer";
 
 const WelcomeLayout = () => {
   return (
     <div>
       <NavBar></NavBar>
-      <p>Welcome Layout</p>
+      <div className="min-h-[calc(100vh-386px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
