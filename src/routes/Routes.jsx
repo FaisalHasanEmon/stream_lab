1baf1160fbea26ccb585325f6b225943b65c2c8c
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-08-25 15:30:12 +0600

    routes page updated

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import WelcomeLayout from "../layouts/WelcomeLayout";
+import SignIn from "../pages/authPages/SignIn/SignIn";
+import SignUp from "../pages/authPages/SignUp/SignUp";
+import VerifyEmail from "../pages/authPages/VerifyEmail/VerifyEmail";
+import ForgetPassOtp from "../pages/authPages/ForgetPassOtp/ForgetPassOtp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <WelcomeLayout />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/verifyEmail",
+    element: <VerifyEmail />,
+  },
+  {
+    path: "/otp",
+    element: <ForgetPassOtp />,
   },
 ]);
 
