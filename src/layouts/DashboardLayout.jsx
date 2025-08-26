5b9695cab2e7389226c2ac5564235cd76137fe34
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-08-26 10:52:56 +0600

    dashboard modified

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const DashboardLayout = () => {
-  return <div className="min-h-screen flex"></div>;
+  return <div className="min-h-screen flex">
+    
+  </div>;
 };
 
 export default DashboardLayout;
